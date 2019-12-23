1ee272f170f05e369a02c85c1435348ffcdecf11
janusz.witrykus <janusz.witrykus@orba.co>
2019-12-23 13:21:44 +0100

    ZZ01-27 #comment Template frontController added

---
+++ templates/frontController/Block/${Controllername}/${Actionname}.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright Copyright © ${commentsYear} ${CommentsCompanyName}. All rights reserved.
+ * @author    ${commentsUserEmail}
+ */
+
+namespace ${Vendorname}\${Modulename}\Block\${Controllername};
+
+use Magento\Framework\View\Element\Template;
+
+class ${Actionname} extends Template
+{
+    /**
+     * @var string $_template
+     */
+    protected $_template = "${Vendorname}_${Modulename}::${controllername}/${actionname}.phtml";
+
+    // write your methods here...
+}
---
+++ templates/frontController/Controller/${Controllername}/${Actionname}.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @copyright Copyright © ${commentsYear} ${CommentsCompanyName}. All rights reserved.
+ * @author    ${commentsUserEmail}
+ */
+ 
+namespace ${Vendorname}\${Modulename}\Controller\${Controllername};
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Action\Context;
+
+class ${Actionname} extends Action
+{
+    /**
+     * @var PageFactory
+     */
+    protected $pageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * {Actionname} Action
+     * 
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Framework\View\Result\Page $resultPage */
+        $resultPage = $this->pageFactory->create();
+        return $resultPage;
+    }
+}
