1e7f85f03c5e31bf45fd2a57c6b34e8616827127
Michał Biarda <michal.biarda@orba.pl>
2019-12-27 10:24:44 +0100

    ZZ01-31 #comment Minor changes to block and viewModel templates

--- templates/block/Block/${Blockname}.php
+++ templates/block/Block/${Blockname}.php
@@ -8,12 +8,7 @@ namespace ${Vendorname}\${Modulename}\Block;
 
 use Magento\Framework\View\Element\Template;
 
-class ${Block_name} extends Template
+class ${Blockname} extends Template
 {
-    //@todo delete / replace this function with your implementation
-    public function getSampleText()
-    {
-        return "Hello World!";
-    }
-    // write your methods here...
+    // Write your methods here...
 }
--- templates/viewModel/ViewModel/${Viewmodelname}.php
+++ templates/viewModel/ViewModel/${Viewmodelname}.php
@@ -8,12 +8,7 @@ namespace ${Vendorname}\${Modulename}\ViewModel;
 
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
-class ${Viewmodel_name} implements ArgumentInterface
+class ${Viewmodelname} implements ArgumentInterface
 {
-    //@todo delete / replace this function with your implementation
-    public function getSampleText()
-    {
-        return "Hello World!";
-    }
-    // write your methods here...
+    // Write your methods here...
 }
