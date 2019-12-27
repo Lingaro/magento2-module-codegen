df482d368c70e5e53489a221a6f955de929cd268
janusz.witrykus <janusz.witrykus@orba.co>
2019-12-27 21:54:05 +0100

    ZZ01-26 #comment Initial commit with old propert names

---
+++ templates/model/Model/${Entityname}SearchResult.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @copyright Copyright © ${commentsYear} ${CommentsCompanyName}. All rights reserved.
+ * @author    ${commentsUserEmail}
+ */
+
+namespace ${Vendorname}\${Modulename}\Model;
+
+use Magento\Framework\Api\SearchResults;
+use ${Vendorname}\${Modulename}\Api\Data\${Entityname}SearchResultInterface;
+
+class ${Entityname}SearchResult extends SearchResults implements ${Entityname}SearchResultInterface
+{
+
+}
---
+++ templates/model/Setup/Uninstall.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @copyright Copyright © ${commentsYear} ${CommentsCompanyName}. All rights reserved.
+ * @author    ${commentsUserEmail}
+ */
+namespace ${Vendorname}\${Modulename}\Setup;
+
+use Magento\Framework\Setup\UninstallInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+
+class Uninstall implements UninstallInterface
+{
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context) //@codingStandardsIgnoreLine
+    {
+        $setup->startSetup();
+
+        if ($setup->tableExists('${vendorname}_${modulename}_${entityname}')) {
+            $setup->getConnection()->dropTable($setup->getTable('${vendorname}_${modulename}_${entityname}'));
+        }
+
+        $setup->endSetup();
+    }
+}
