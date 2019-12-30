184db3da6c5e22d181c840e382084180d68c3a83
janusz.witrykus <janusz.witrykus@orba.co>
2019-12-30 12:03:08 +0100

    ZZ01-27 viewModel in place of Block, merge config for routes

--- templates/frontController/Controller/${Controllername}/${Actionname}.php
+++ templates/frontController/Controller/${Controllername}/${Actionname}.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @copyright Copyright © ${commentsYear} ${CommentsCompanyName}. All rights reserved.
  * @author    ${commentsUserEmail}
---
+++ templates/frontController/ViewModel/${Actionname}.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @copyright Copyright © ${commentsYear} ${CommentsCompanyName}. All rights reserved.
+ * @author    ${commentsUserEmail}
+ */
+
+namespace ${Vendorname}\${Modulename}\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class ${Actionname} implements ArgumentInterface
+{
+    // Write your methods here...
+}
