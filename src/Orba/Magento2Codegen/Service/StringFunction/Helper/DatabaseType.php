60dc15578df655090e48375eaf8f71c4a3a68f83
Michał Biarda <michal.biarda@orba.co>
2020-01-14 14:47:41 +0100

    ZZ01-26 #comment Refactoring and unit tests for FiltersExtension

---
+++ src/Orba/Magento2Codegen/Service/StringFunction/Helper/DatabaseType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Orba\Magento2Codegen\Service\StringFunction\Helper;
+
+use InvalidArgumentException;
+
+class DatabaseType
+{
+    const INT_TYPES = ['int', 'smallint', 'bigint', 'tinyint', 'timestamp'];
+    const DECIMAL_TYPES = ['decimal', 'float', 'double'];
+    const BOOLEAN_TYPES = ['boolean'];
+    const STRING_TYPES = ['blob', 'text', 'varbinary', 'varchar'];
+    const ALLOWED_TYPES = ['bigint', 'blob', 'boolean', 'date', 'datetime',
+        'decimal', 'double', 'float', 'int', 'smallint', 'text', 'timestamp', 'tinyint',
+        'varbinary', 'varchar'];
+
+    public function normalize(string $databaseType): string
+    {
+        return strtolower($databaseType);
+    }
+
+    public function validate(string $databaseType): void
+    {
+        if (!in_array($databaseType, self::ALLOWED_TYPES)) {
+            throw new InvalidArgumentException('Wrong database type: ' . $databaseType
+                . ' Allowed types: ' . implode(",", self::ALLOWED_TYPES));
+        }
+    }
+}
