926edd0540ad092245005c16483944669a4dcce5
Dawid Cieśliczka <dawid.ciesliczka@orba.co>
2020-07-03 12:12:09 +0200

    ZZ01-69 js mixin templates

--- templates/jsMixin/view/{{ area }}/web/js/{{ originalModule|kebab }}-mixin.js
+++ templates/jsMixin/view/{{ area }}/web/js/{{ originalModule|kebab }}-mixin.js
@@ -1,15 +1,36 @@
-define({{ ("jQuery widget" == type) ? "['jquery']," : "" }}function ($) {
+define([
+{% if type == 'jQuery widget' %}
+    'jquery',
+    'jquery/ui'
+{% elseif (type == 'plain' and extendMethod) %}
+    'mage/utils/wrapper'
+{% endif %}
+    ], function ({% if type == 'jQuery widget' %}${%elseif (type == 'plain' and extendMethod) %}wrapper, {{methodName}}{%endif%}) {
     'use strict';
+{% if ("jQuery widget" == type) %}
 
-    {{ methodName }}: function() {
+{{ extendMethod ? "return function (originalWidget) {" : "" }}
+    $.widget('{{ methodName }}', originalWidget, {});
 
-    };
+    return $.{{ methodName }}
+{{ extendMethod ? "};" : "" }}
+{% elseif "component" == type %}
 
+    return function (Component) {
+        return Component.extend({
+        {{ methodName }}: function () {
+            {{ extendMethod ? "this._super();" : "" }}
+            }
+        });
+    }
+{% else %}
+
+    return function ({{ methodName }}) {
 {% if extendMethod %}
-    return function (targetWidget) {
-        $.widget('mage.modal', targetWidget, methodName);
-
-        return $.mage.modal;
+        return wrapper.wrap({{ methodName }}, function (original, config, element) {
+            original(config, element);
+        });
+{% endif %}
     };
 {% endif %}
 });
