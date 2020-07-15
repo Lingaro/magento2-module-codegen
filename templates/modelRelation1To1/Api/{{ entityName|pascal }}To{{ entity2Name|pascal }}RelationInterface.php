ae237f245e04d318ddab71b41681a76dff058754
Mateusz Ścisłowski <mateusz.scislowski@orba.co>
2020-07-15 11:56:39 +0200

    ZZ01-90 1to1 template added

---
+++ templates/modelRelation1To1/Api/{{ entityName|pascal }}To{{ entity2Name|pascal }}RelationInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @copyright Copyright © {{ commentsYear }} {{ commentsCompanyName }}. All rights reserved.
+ * @author    {{ commentsUserEmail }}
+ */
+
+namespace {{ vendorName|pascal }}\{{ moduleName|pascal }}\Api;
+
+use Magento\Framework\Exception\NoSuchEntityException;
+use {{ vendorName|pascal }}\{{ moduleName|pascal }}\Api\Data\{{ entityName|pascal }}Interface;
+use {{ vendorName|pascal }}\{{ moduleName|pascal }}\Api\Data\{{ entity2Name|pascal }}Interface;
+
+interface {{ entityName|pascal }}To{{ entity2Name|pascal }}RelationInterface
+{
+    /**
+    * @param {{ entityName|pascal }}Interface ${{ entityName|camel }}
+    * @return {{ entity2Name|pascal }}Interface|null
+    */
+    public function get{{ entity2Name|pascal }}({{ entityName|pascal }}Interface ${{ entityName|camel }}): ?{{ entity2Name|pascal }}Interface;
+
+    /**
+     * @param {{ entity2Name|pascal }}Interface ${{ entity2Name|camel }}
+     * @return {{ entityName|pascal }}Interface|null
+     */
+    public function get{{ entityName|pascal }}({{ entity2Name|pascal }}Interface ${{ entity2Name|camel }}): ?{{ entityName|pascal }}Interface;
+
+}
---
+++ templates/modelRelation1To1/Model/{{ entity2Name|pascal }}.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @copyright Copyright © {{ commentsYear }} {{ commentsCompanyName }}. All rights reserved.
+ * @author    {{ commentsUserEmail }}
+ */
+
+namespace {{ vendorName|pascal }}\{{ moduleName|pascal }}\Model;
+
+use Magento\Framework\Model\AbstractModel;
+use {{ vendorName|pascal }}\{{ moduleName|pascal }}\Api\Data\{{ entityName|pascal }}Interface;
+use {{ vendorName|pascal }}\{{ moduleName|pascal }}\Model\ResourceModel\{{ entityName|pascal }} as {{ entityName|pascal }}ResourceModel;
+
+class {{ entity2Name|pascal }} extends AbstractModel implements {{ entity2Name|pascal }}Interface
+{
+
+    /**
+     * @return int|null
+     */
+    public function get{{ entityName|pascal }}Id()
+    {
+        return $this->getData('{{ entityName|snake }}_id');
+    }
+
+    /**
+     * @param int $value
+     * @return void
+     */
+    public function set{{ entityName|pascal }}Id($value)
+    {
+        $this->setData('{{ entityName|snake }}_id', $value);
+    }
+
+}
