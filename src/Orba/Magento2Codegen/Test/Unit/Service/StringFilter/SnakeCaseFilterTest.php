ab3da411964e6a7ebaa23b295c19c7ef0f6c20d1
Marcin Korecki <marcin.korecki@orba.co>
2020-07-23 12:18:07 +0200

    ZZ01-100 #comment Added word separation with "_" when the first word ends with and the second begins with a capital letter, eg. "NToN" is changed to "n_to_n" instead of "nto_n"

--- src/Orba/Magento2Codegen/Test/Unit/Service/StringFilter/SnakeCaseFilterTest.php
+++ src/Orba/Magento2Codegen/Test/Unit/Service/StringFilter/SnakeCaseFilterTest.php
@@ -39,7 +39,16 @@ class SnakeCaseFilterTest extends TestCase
             ['one1Two2', 'one1_two2'],
             ['_one_', 'one'],
             ['one__two', 'one_two'],
-            [' one  two ', 'one_two']
+            [' one  two ', 'one_two'],
+            ['ModelNToN', 'model_n_to_n'],
+            ['ModeLNToN', 'mode_ln_to_n'],
+            ['Model NToN', 'model_n_to_n'],
+            ['ModelN ToN', 'model_n_to_n'],
+            ['modelNTON', 'model_nton'],
+            ['modelNTo10TH', 'model_n_to10_th'],
+            ['model1ToN', 'model1_to_n'],
+            ['model1TON', 'model1_ton'],
+            ['EMAsMOTHER', 'em_as_mother'],
         ];
     }
 }
