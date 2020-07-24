f3aa7a592a5f0c9e298f655c9789f954c54ab3f7
Michał Biarda <michal.biarda@orba.co>
2020-07-24 11:18:05 +0000

    Merged in bugfix/ZZ01-100-merge-to-dev (pull request #93)
    Bugfix/ZZ01-100 merge to dev
    
    * ZZ01-100 #comment Removed changing the second capital letter to lowercase when 2 capital letters are placed next to each other, eg. "NToN" is not changed to "NtoN"
    
    * ZZ01-100 #comment Added word separation with "_" when the first word ends with and the second begins with a capital letter, eg. "NToN" is changed to "n_to_n" instead of "nto_n"
    
    * ZZ01-100 #comment Minor unit tests polishing

--- src/Orba/Magento2Codegen/Service/StringFilter/CamelCaseFilter.php
+++ src/Orba/Magento2Codegen/Service/StringFilter/CamelCaseFilter.php
@@ -8,6 +8,7 @@ class CamelCaseFilter implements FilterInterface
     {
         $text = preg_replace('/[^a-zA-Z0-9]/', ' ', $text);
         $text = preg_replace('/([a-z0-9])([A-Z])/', '$1 $2', $text);
+        $text = preg_replace('/([A-Z])([A-Z][a-z0-9])/', '$1 $2', $text);
         $text = ucwords(strtolower($text));
         $text = str_replace(' ', '', $text);
         $text = lcfirst($text);
