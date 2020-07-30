c96335e8ca4549aa855095a86d7f933152eb3dcc
Pavlo Vasheniuk <pavlo.vasheniuk@orba.co>
2020-07-30 20:20:37 +0200

    ZZ01-108 added unit test to php declare cleaner class

---
+++ src/Orba/Magento2Codegen/Test/Unit/Service/FileMerger/PhpMerger/NodeTree/NodeCleaner/DeclareCleanerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Orba\Magento2Codegen\Test\Unit\Service\FileMerger\PhpMerger\NodeTree\NodeCleaner;
+
+use Orba\Magento2Codegen\Test\Unit\TestCase;
+use Orba\Magento2Codegen\Service\FileMerger\PhpMerger\NodeTree\NodeCleaner\DeclareCleaner;
+use Orba\Magento2Codegen\Service\FileMerger\PhpMerger\NodeTree\NameResolver\NodeChainNameResolver;
+use PhpParser\Node\Stmt\Declare_;
+use PHPUnit\Framework\MockObject\MockObject;
+
+class DeclareCleanerTest extends TestCase
+{
+    /**
+     * @var DeclareCleaner
+     */
+    private $declareCleaner;
+
+    /**
+     * @var MockObject|NodeChainNameResolver
+     */
+    private $nodeChainNameResolver;
+
+    public function setUp(): void
+    {
+        $this->nodeChainNameResolver = $this->createPartialMock(
+            NodeChainNameResolver::class,
+            ['resolve']
+        );
+        $this->declareCleaner = new DeclareCleaner($this->nodeChainNameResolver);
+    }
+
+    public function testCleanTwoSimilarDeclaresResultInOne(): void
+    {
+        $parserNode = new Declare_([]);
+
+        $nodes = [$parserNode, $parserNode];
+        $expectedResult = ['resolved_name' => $parserNode];
+
+        $this->nodeChainNameResolver->expects($this->any())
+            ->method('resolve')
+            ->willReturn('resolved_name');
+
+        $actualResult = $this->declareCleaner->clean($nodes);
+        $this->assertSame($expectedResult, $actualResult);
+    }
+}
