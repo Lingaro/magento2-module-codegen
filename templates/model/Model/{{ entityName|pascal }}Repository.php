d44c851f6d74ac77ebf069069e8d1a380698b7d0
Aliasgar Bharmal <aliasgar.bharmal@orba.co>
2021-08-10 09:45:05 +0000

    Merged in ZZ01-128-use-collection-processor-for-ge (pull request #126)
    changed getList() function using collectionProcessorInterface
    
    * changed getList() function using collectionProcessorInterface
    
    * Merged dev into ZZ01-128-use-collection-processor-for-ge
    * #ZZ01-128 Remove unused "use" from repository class in "model" template
    
    
    Approved-by: Michał Biarda

--- templates/model/Model/{{ entityName|pascal }}Repository.php
+++ templates/model/Model/{{ entityName|pascal }}Repository.php
@@ -10,31 +10,31 @@ declare(strict_types=1);
 namespace {{ vendorName|pascal }}\{{ moduleName|pascal }}\Model;
 
 use {{ vendorName|pascal }}\{{ moduleName|pascal }}\Api\{{ entityName|pascal }}RepositoryInterface;
-use Exception;
 use Magento\Framework\Api\SearchCriteriaInterface;
-use Magento\Framework\Api\SortOrder;
-use Magento\Framework\Exception\AlreadyExistsException;
+use Magento\Framework\Api\SearchCriteria\CollectionProcessorInterface;
 use Magento\Framework\Exception\NoSuchEntityException;
 use {{ vendorName|pascal }}\{{ moduleName|pascal }}\Api\Data\{{ entityName|pascal }}Interface;
 use {{ vendorName|pascal }}\{{ moduleName|pascal }}\Api\Data\{{ entityName|pascal }}SearchResultInterface;
 use {{ vendorName|pascal }}\{{ moduleName|pascal }}\Api\Data\{{ entityName|pascal }}SearchResultInterfaceFactory;
 use {{ vendorName|pascal }}\{{ moduleName|pascal }}\Model\ResourceModel\{{ entityName|pascal }}\CollectionFactory as {{ entityName|pascal }}CollectionFactory;
-use {{ vendorName|pascal }}\{{ moduleName|pascal }}\Model\ResourceModel\{{ entityName|pascal }}\Collection;
 
 class {{ entityName|pascal }}Repository implements {{ entityName|pascal }}RepositoryInterface
 {
     private {{ entityName|pascal }}Factory ${{ entityName|camel }}Factory;
     private {{ entityName|pascal }}CollectionFactory ${{ entityName|camel }}CollectionFactory;
     private {{ entityName|pascal }}SearchResultInterfaceFactory $searchResultFactory;
+    private CollectionProcessorInterface $collectionProcessor;
 
     public function __construct(
         {{ entityName|pascal }}Factory ${{ entityName|camel }}Factory,
         {{ entityName|pascal }}CollectionFactory ${{ entityName|camel }}CollectionFactory,
-        {{ entityName|pascal }}SearchResultInterfaceFactory ${{ entityName|camel }}SearchResultInterfaceFactory
+        {{ entityName|pascal }}SearchResultInterfaceFactory ${{ entityName|camel }}SearchResultInterfaceFactory,
+        CollectionProcessorInterface $collectionProcessor
     ) {
         $this->{{ entityName|camel }}Factory = ${{ entityName|camel }}Factory;
         $this->{{ entityName|camel }}CollectionFactory = ${{ entityName|camel }}CollectionFactory;
         $this->searchResultFactory = ${{ entityName|camel }}SearchResultInterfaceFactory;
+        $this->collectionProcessor = $collectionProcessor;
     }
 
     public function getById(int $id): {{ entityName|pascal }}Interface
@@ -62,48 +62,11 @@ class {{ entityName|pascal }}Repository implements {{ entityName|pascal }}Reposi
     public function getList(SearchCriteriaInterface $searchCriteria): {{ entityName|pascal }}SearchResultInterface
     {
         $collection = $this->{{ entityName|camel }}CollectionFactory->create();
-        $this->addFiltersToCollection($searchCriteria, $collection);
-        $this->addSortOrdersToCollection($searchCriteria, $collection);
-        $this->addPagingToCollection($searchCriteria, $collection);
-        $collection->load();
-        return $this->buildSearchResult($searchCriteria, $collection);
-    }
-
-    private function addFiltersToCollection(SearchCriteriaInterface $searchCriteria, Collection $collection): void
-    {
-        foreach ($searchCriteria->getFilterGroups() as $filterGroup) {
-            $fields = $conditions = [];
-            foreach ($filterGroup->getFilters() as $filter) {
-                $fields[] = $filter->getField();
-                $conditions[] = [$filter->getConditionType() => $filter->getValue()];
-            }
-            $collection->addFieldToFilter($fields, $conditions);
-        }
-    }
-
-    private function addSortOrdersToCollection(SearchCriteriaInterface $searchCriteria, Collection $collection): void
-    {
-        foreach ((array) $searchCriteria->getSortOrders() as $sortOrder) {
-            $direction = $sortOrder->getDirection() == SortOrder::SORT_ASC ? 'asc' : 'desc';
-            $collection->addOrder($sortOrder->getField(), $direction);
-        }
-    }
-
-    private function addPagingToCollection(SearchCriteriaInterface $searchCriteria, Collection $collection): void
-    {
-        $collection->setPageSize($searchCriteria->getPageSize());
-        $collection->setCurPage($searchCriteria->getCurrentPage());
-    }
-
-    private function buildSearchResult(
-        SearchCriteriaInterface $searchCriteria,
-        Collection $collection
-    ): {{ entityName|pascal }}SearchResultInterface {
-        /** @var {{ entityName|pascal }}SearchResultInterface $searchResults */
-        $searchResults = $this->searchResultFactory->create();
-        $searchResults->setSearchCriteria($searchCriteria);
-        $searchResults->setItems($collection->getItems());
-        $searchResults->setTotalCount($collection->getSize());
-        return $searchResults;
+        $this->collectionProcessor->process($searchCriteria, $collection);
+        $searchResult = $this->searchResultFactory->create();
+        $searchResult->setSearchCriteria($searchCriteria);
+        $searchResult->setItems($collection->getItems());
+        $searchResult->setTotalCount($collection->getSize());
+        return $searchResult;
     }
 }
